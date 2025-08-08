c347aee8188806a7578e81c820462f0490404997
gouravchauhan712 <117297226+gouravchauhan712@users.noreply.github.com>
2025-08-08 14:47:42 +0530

    Update app.py

--- churn_prediction_project/app.py
+++ churn_prediction_project/app.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+model_path = os.path.join(BASE_DIR, "logistic_regression_model.pkl")
+scaler_path = os.path.join(BASE_DIR, "scaler.pkl") 
 
 # Load saved model and scaler
-model = joblib.load("logistic_regression_model.pkl")
-scaler = joblib.load("scaler.pkl")
+# model = joblib.load("logistic_regression_model.pkl")
+# scaler = joblib.load("scaler.pkl")
 
 # Get expected feature names from the model itself
 feature_names = model.feature_names_in_.tolist()  # ✅ No need to load separately
@@ -64,3 +69,4 @@ if st.button("Predict Churn"):
         st.error(f"❌ The customer is likely to churn.\n\nChurn Probability: **{probability:.2%}**")
     else:
         st.success(f"✅ The customer is not likely to churn.\n\nChurn Probability: **{probability:.2%}**")
+
